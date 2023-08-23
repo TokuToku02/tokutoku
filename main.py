60dca6cb30174e39e64fecf78344791812c97c12
fclef <fclef@fclef.jp>
2023-08-23 11:44:51 +0900

    basic authorization

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from bottle import default_app, get, post, run, HTTPResponse, request, TEMPLATE_PATH, jinja2_template as template, static_file, route, redirect
+from bottle import default_app, get, post, run, HTTPResponse, request, TEMPLATE_PATH, jinja2_template as template, \
+    static_file, route, redirect, auth_basic
 from dotenv import load_dotenv
 import os
 import openai
@@ -27,6 +28,15 @@ class Context:
         return self.context
 
 
+def basic_authorization(input_id, input_password):
+    with open("./basic.txt", mode="r") as basic:
+        try:
+            basic_id, basic_password = basic.readline().split(':')
+            return input_id == basic_id and input_password == basic_password
+        except ValueError:
+            return True
+
+
 @route('/static/<file_path:path>')
 def static(file_path):
     return static_file(file_path, root='./static')
@@ -40,23 +50,32 @@ def response_for_options(**kwargs):
     if 'Origin' in request.headers.keys():
         re.set_header('Access-Control-Allow-Origin', request.headers['Origin'])
         re.set_header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
-        re.set_header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token, Authorization')
+        re.set_header('Access-Control-Allow-Headers',
+                      'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token, Authorization')
 
     return re
 
 
 @app.route("/")
+@auth_basic(basic_authorization)
 def index():
-    context = Context()
-    context.add("msg", "hello world")
-    return template("index.html", ctx=context.to_template())
+    return template("index.html")
 
 
 @get("/setting")
+@auth_basic(basic_authorization)
 def setting():
     context = Context()
     with open("./rule.txt", mode="r") as rule_file:
         context.add("rule", "".join(rule_file.readlines()))
+        with open("./basic.txt", mode="r") as basic:
+            try:
+                basic_id, basic_password = basic.readline().split(':')
+            except ValueError:
+                basic_id = ''
+                basic_password = ''
+            context.add("basic_id", basic_id)
+            context.add("basic_password", basic_password)
     return template("setting.html", ctx=context.to_template())
 
 
@@ -65,6 +84,14 @@ def setting():
     rule = request.forms.rule
     with open("./rule.txt", mode="w") as rule_file:
         rule_file.writelines(rule.split("\n"))
+
+    basic_id = request.forms.basic_id
+    basic_password = request.forms.basic_password
+    with open("./basic.txt", mode="w") as basic:
+        if basic_id and basic_password:
+            basic.writelines([basic_id + ':' + basic_password])
+        else:
+            basic.writelines([])
     redirect('/')
 
 
@@ -103,6 +130,7 @@ def ingen():
         return template("result.html", ctx=context.to_template())
     except Exception as e:
         return e.error.message
+
 
 if __name__ == "__main__":
     run(host="0.0.0.0", port=8000)
